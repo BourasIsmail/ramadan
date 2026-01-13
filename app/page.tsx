48816de66f7ac460c771496a1f18c604ba654940
ismail <ismailbouras1968@gmail.com>
2026-01-13 10:46:12 +0100

    dash 2026

--- app/page.tsx
+++ app/page.tsx
@@ -1,15 +1,15 @@
 import Dashboard from "@/components/Dashboard";
 
 export default function Home() {
-    return (
-        <main className="container mx-auto p-4">
-            <img src="/img.png" alt="Logo" className="h-12 w-30 " />
-            <div className="flex items-center justify-center mb-4">
-                <h1 className="text-3xl text-purple-900 font-bold text-center">
-                    Opération Ramadan 2025
-                </h1>
-            </div>
-            <Dashboard />
-        </main>
-    );
+  return (
+    <main className="container mx-auto p-4">
+      <img src="/img.png" alt="Logo" className="h-12 w-30 " />
+      <div className="flex items-center justify-center mb-4">
+        <h1 className="text-3xl text-purple-900 font-bold text-center">
+          Opération Ramadan 2026
+        </h1>
+      </div>
+      <Dashboard />
+    </main>
+  );
 }
